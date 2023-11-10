10c367c1c1d6b5bea948901deae9d4c13cf2d39e
SlamChillz <mendyslam@gmail.com>
2023-11-10 16:37:24 +0100

    changed config setup

--- api/main_test.go
+++ api/main_test.go
@@ -11,14 +11,8 @@ import (
 )
 
 func newTestServer(t *testing.T, store db.Store) *Server {
-	config := utils.Config{
-		BITPOWR_ACCOUNT_ID: os.Getenv("BITPOWR_ACCOUNT_ID"),
-		BITPOWR_API_KEY: os.Getenv("BITPOWR_API_KEY"),
-		SHUTTER_PUBLIC_KEY: os.Getenv("SHUTTER_PUBLIC_KEY"),
-		VALIDATE_BANK_URL: os.Getenv("VALIDATE_BANK_URL"),
-		JWT_SECRET: os.Getenv("JWT_SECRET"),
-		JWT_ACCESS_TOKEN_DURATION: 3,
-	}
+	config, err := utils.LoadConfig("../")
+	require.NoError(t, err)
 	server, err := NewServer(config, store)
 	require.NoError(t, err)
 	return server
