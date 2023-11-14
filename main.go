86ccc774fa3c72a8797e98a822cf51ed370893d9
SlamChillz <mendyslam@gmail.com>
2023-11-14 17:23:41 +0100

    start grpc server function

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net"
 
 	_ "github.com/lib/pq"
 	"github.com/slamchillz/xchange/api"
 	db "github.com/slamchillz/xchange/db/sqlc"
+	"github.com/slamchillz/xchange/gapi"
+	"github.com/slamchillz/xchange/pb"
 	"github.com/slamchillz/xchange/utils"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -19,9 +24,36 @@ func main() {
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
-	server, err := api.NewServer(config, db.NewStorage(conn))
+	store := db.NewStorage(conn)
+	runGRPCServer(config, store)
+}
+
+func runGRPCServer(config utils.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
 	if err != nil {
-		log.Fatal("cannot create server:", err)
+		log.Fatal("cannot create gapi server:", err)
 	}
-	log.Fatal(server.Start(":8080"))
+	grpcServer := grpc.NewServer()
+	pb.RegisterXchangeServer(grpcServer, server)
+	reflection.Register(grpcServer)
+	listener, err := net.Listen("tcp", config.GRPCServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener for GRPC server:", err)
+	}
+	log.Printf("starting gRPC server at %s", config.GRPCServerAddress)
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot start gRPC server:", err)
+	}
+}
+
+func runHTTPServer(config utils.Config, store db.Store) {
+	server, err := api.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create HTTP server:", err)
+	}
+	err = server.Start(config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal("cannot start HTTP server:", err)
+	}
 }
