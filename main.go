53247205ad0ac5cbc2505dd76ed3780f9e42ed5b
SlamChillz <mendyslam@gmail.com>
2023-11-15 16:56:43 +0100

    gateway server start up func

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"log"
 	"net"
+	"net/http"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"github.com/slamchillz/xchange/api"
 	db "github.com/slamchillz/xchange/db/sqlc"
@@ -12,7 +15,9 @@ import (
 	"github.com/slamchillz/xchange/pb"
 	"github.com/slamchillz/xchange/utils"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func main() {
@@ -25,7 +30,9 @@ func main() {
 		log.Fatal("cannot connect to db:", err)
 	}
 	store := db.NewStorage(conn)
-	runGRPCServer(config, store)
+	go runGRPCServer(config, store)
+	runGatewayServer(config)
+	// runHTTPServer(config, store)
 }
 
 func runGRPCServer(config utils.Config, store db.Store) {
@@ -55,5 +62,36 @@ func runHTTPServer(config utils.Config, store db.Store) {
 	err = server.Start(config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("cannot start HTTP server:", err)
+	}
+}
+
+func runGatewayServer(config utils.Config) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	opts := runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+		MarshalOptions: protojson.MarshalOptions{
+			UseProtoNames: true,
+		},
+		UnmarshalOptions: protojson.UnmarshalOptions{
+			DiscardUnknown: true,
+		},
+	})
+	grpcServerMux := runtime.NewServeMux(opts)
+	err := pb.RegisterXchangeHandlerFromEndpoint(ctx, grpcServerMux, config.GRPCServerAddress, []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	})
+	if err != nil {
+		log.Fatal("cannot register gateway server:", err)
+	}
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcServerMux)
+	listener, err := net.Listen("tcp", config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener for gateway server:", err)
+	}
+	log.Printf("starting gateway server at %s", config.HTTPServerAddress)
+	err = http.Serve(listener, mux)
+	if err != nil {
+		log.Fatal("cannot start gateway server:", err)
 	}
 }
